bd9f1fa54286c5be3e3b3cae85a23517241c36c3
Antoine MEVEL <mevelantoi@eisti.eu>
2021-10-04 10:41:25 +0200

    Implémentation de l'interface graphique

--- creerAttestation.py
+++ creerAttestation.py
@@ -1,12 +1,69 @@
 import tkinter as tk
-import qrcode as qr
+#import qrcode as qr
 import os
+from tkinter.constants import CENTER
+from typing import Text
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-os.chdir("H:\Documents\ING3 - CS\Crypto\projet-crypto-diplome")
+def validate(window):
+    if (True):
+         window.destroy()
+    else:
+        pass
+# Regex check email : .*\@.*\.com
 
+#Fenêtre racine
+root = tk.Tk()
+root.title("Application de création de diplôme")
+root.geometry("1600x900+100+50")
+
+##Fenêtre de demande d'OTP initiale
+otpWindow= tk.Toplevel(root)
+otpWindow.geometry("250x100+750+400")
+otpWindow.attributes('-topmost', True)
+
+textOTP = tk.Label(otpWindow,text="Entrez votre OTP")
+inputOTP = tk.Entry(otpWindow)
+buttonOTP = tk.Button(otpWindow,text="Valider",command=otpWindow.destroy)
+
+textOTP.place(relx=0.5, rely=0.1, anchor=CENTER)
+inputOTP.place(relx=0.5, rely=0.25, anchor=CENTER)
+buttonOTP.place(relx=0.5, rely=0.5,anchor=CENTER)
+otpWindow.grab_set()
+
+##Elements de la fenêtre racine
+textNom=tk.Label(root,text="Nom :")
+inputNom = tk.Entry(root)
+
+textPrenom=tk.Label(root,text="Prénom :")
+inputPrenom = tk.Entry(root)
+
+formation = tk.StringVar()
+optionsFormations=['ING1 GI', 'ING1 GM',"ING2 GSI", "ING2 SIE","ING3 CS","ING3 INEM","ING3 VISUAL"]
+textFormations = tk.Label(root,text="Formation : ")
+dropFormations = tk.OptionMenu(root, formation, *optionsFormations)
+
+textMail = tk.Label(root,text="Adresse e-mail :",pady=30)
+inputMail = tk.Entry(root)
+
+buttonValider = tk.Button(root,text="Valider")
+
+textNom.grid(row=0,column=0)
+inputNom.grid(row=0,column=1)
+textPrenom.grid(row=1,column=0)
+inputPrenom.grid(row=1,column=1)
+textFormations.grid(row=2,column=0)
+dropFormations.grid(row=2,column=1)
+textMail.grid(row=3,column=0)
+inputMail.grid(row=3,column=1)
+
+buttonValider.grid(row=10,column=0)
+
+root.mainloop()
+
+"""
 print("Entrez votre OTP : ")
 otpEcole = input()
 
@@ -33,3 +90,5 @@ if (True): #Ajouter l'application d'OTP
 
 else:
     print("Accès refusé")
+
+"""
--- otp_google.py
+++ otp_google.py
@@ -1,26 +1,26 @@
 import hmac, base64, struct, hashlib, random
 
+class OTP : 
+    def get_hotp_token(secret, intervals_no):
+        key = base64.b32decode(secret, True)
+        msg = struct.pack(">Q", intervals_no)
+        h = hmac.new(key, msg, hashlib.sha1).digest()
+        o = o = h[19] & 15
+        h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
+        return h
 
-def get_hotp_token(secret, intervals_no):
-    key = base64.b32decode(secret, True)
-    msg = struct.pack(">Q", intervals_no)
-    h = hmac.new(key, msg, hashlib.sha1).digest()
-    o = o = h[19] & 15
-    h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
-    return h
+    def check_otp(otp):
+        test_otp = input("Entrez votre OTP : ")
+        if test_otp == str(otp):
+            print("OTP correct")
+        else:
+            print("Erreur")
 
-def check_otp(otp):
-    test_otp = input("Entrez votre OTP : ")
-    if test_otp == str(otp):
-        print("OTP correct")
-    else:
-        print("Erreur")
-
-secret = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for _ in range(16))
-otp = get_hotp_token(secret, intervals_no=1)
-f = open("otp.txt","w")
-f.write(str(otp))
-f.close()
+    secret = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for _ in range(16))
+    otp = get_hotp_token(secret, intervals_no=1)
+    f = open("otp.txt","w")
+    f.write(str(otp))
+    f.close()
 
 print ("Votre OTP est "+ str(otp))
 #check_otp(otp)
